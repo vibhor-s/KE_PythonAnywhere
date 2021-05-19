bdc046782241b78ff681cb0a9676fc172d773de3
Vibhor <vibhorshukla97@gmail.com>
2021-05-19 23:28:23 +0530

    Optimized product list and routed product category

--- krishna_electronics/shop/views.py
+++ krishna_electronics/shop/views.py
@@ -5,14 +5,20 @@ from itertools import chain
 
 # Create your views here.
 def index(request):
+    all_products = Product.objects.all();
     ac = Product.objects.filter(product_category='AC')
     ledtv = Product.objects.filter(product_category='LED TV')
     allprods = list(chain(ac, ledtv))
-
-
-
-    params = {'allprods' : allprods}
+    category_list = []
+    for i in all_products:
+        if(i.product_category not in category_list):
+            category_list.append(i.product_category)
+    category_list = sorted(category_list)
+    params = {'allprods' : allprods, 'category_list' : category_list}
     return render(request, 'shop/home.html', params)
+
+
+
 
 
 def product_view (request, my_id):
@@ -22,7 +28,11 @@ def product_view (request, my_id):
 def about(request):
     return render(request, 'shop/about.html')
 
-
+def viewall(request, my_category):
+    temp = my_category
+    product_cat = Product.objects.filter(product_category = my_category)
+    params = {'product_cat' : product_cat, 'temp':temp}
+    return render(request, 'shop/viewall.html', params)
 
 
 
